23567908578a426134886e3c07e50a698f6988a4
Leeor Vardi <leeor.v@qualisystems.com>
2021-04-22 15:38:13 +0300

    trying to convert sandbox id to workable string

--- lib/CloudShellAPILibrary.py
+++ lib/CloudShellAPILibrary.py
@@ -4,12 +4,12 @@ import json
 import time
 from cloudshell.api.cloudshell_api import CloudShellAPISession
 from robot.api import logger
+from uuid import UUID
 
 
 class CloudShellAPILibrary(object):
     def __init__(self, cloudshell_address, user="admin", auth_token='', domain="Global"):
         self.api_session = CloudShellAPISession(cloudshell_address, user, token_id=auth_token, domain=domain)
 
-    def write_message(self, sandbox_id : str, message):
-        #self.api_session.WriteMessageToReservationOutput(str(sandbox_id), message)
-        print(f"Sandbox ID:{sandbox_id} {type(sandbox_id)}, message:{message}")
+    def write_message(self, sandbox_id : UUID, message):
+        self.api_session.WriteMessageToReservationOutput(str(sandbox_id), message)
