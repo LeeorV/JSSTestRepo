554f0f99108cb2a4f71b9bb81328a6cd6fbb24c4
Leeor Vardi <leeor.v@qualisystems.com>
2021-04-22 14:27:40 +0300

    fixed variable name on line 11 in CloudShellAPILIbrary.py

--- lib/CloudShellAPILibrary.py
+++ lib/CloudShellAPILibrary.py
@@ -8,7 +8,7 @@ from robot.api import logger
 
 class CloudShellAPILibrary(object):
     def __init__(self, cloudshell_address, user="admin", auth_token='', domain="Global"):
-        self.api_session = CloudShellAPISession(cloudshell_address, user, token_id=token, domain=domain)
+        self.api_session = CloudShellAPISession(cloudshell_address, user, token_id=auth_token, domain=domain)
 
     def write_message(self, sandbox_id, message):
         self.api_session.WriteMessageToReservationOutput(sandbox_id, message)
