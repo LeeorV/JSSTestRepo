19c1ec9ab391eac735005d0998cc514d1c6cace3
Leeor Vardi <leeor.v@qualisystems.com>
2021-04-22 14:24:25 +0300

    Fixed python syntax in library CloudShellAPILibrary

--- lib/CloudShellAPILibrary.py
+++ lib/CloudShellAPILibrary.py
@@ -7,7 +7,7 @@ from robot.api import logger
 
 
 class CloudShellAPILibrary(object):
-    def __init__(self, cloudshell_address, user="admin", auth_token, domain="Global"):
+    def __init__(self, cloudshell_address, user="admin", auth_token='', domain="Global"):
         self.api_session = CloudShellAPISession(cloudshell_address, user, token_id=token, domain=domain)
 
     def write_message(self, sandbox_id, message):
